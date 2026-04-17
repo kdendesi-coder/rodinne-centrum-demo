1e390b4df6c7b363fefcc49e1cf9c53ddafa6f23
kdendesi-coder <kdendesi@gmail.com>
2026-04-17 17:08:18 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,15 +17,14 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
             <Route path="/aktivita/:activityId" element={<ActivityDetail />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </AuthProvider>
   </QueryClientProvider>
