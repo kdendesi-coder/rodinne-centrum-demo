a258629d74a4ded18d605b0f50399c62373b4e2a
kdendesi-coder <kdendesi@gmail.com>
2026-04-17 16:01:08 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -61,7 +61,7 @@ const Navigation = () => {
                     {link.label}
                     {link.dropdown && (
                       <img 
-                        src="https://rodinne-centrum-demo.vercel.app/icons/down-arrow.png"
+                        src="https://rodinne-centrum-demo.vercel.app/icons/down-arrow.jpg"
                         alt="down-arrow"
                         className="w-5 h-5 transition-transform duration-200 group-hover:rotate-180"
                         />
