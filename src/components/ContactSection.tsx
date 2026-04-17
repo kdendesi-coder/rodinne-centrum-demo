4e58efc07e79991c07dfd792bfd871054d2bd806
kdendesi-coder <kdendesi@gmail.com>
2026-04-17 17:32:44 +0200

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -162,7 +162,7 @@ const ContactSection = () => {
           </div>
 
           {/* Map */}
-          <div className="relative group bg-muted rounded-3xl overflow-hidden h-[260px] md:h-[340px] flex items-center justify-center">
+          <div className="relative group bg-muted rounded-3xl overflow-hidden h-[550px] md:h-[340px] flex items-center justify-center">
             {mapImage ? (
               <img src={mapImage} alt="Map" className="w-full h-full object-cover" />
             ) : (
