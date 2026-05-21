699cc33db53a6a0f83f8245361c7e54d298d4fda
kdendesi-coder <kdendesi@gmail.com>
2026-05-21 17:33:44 +0200

    Update ActivitiesSection.tsx

--- src/components/ActivitiesSection.tsx
+++ src/components/ActivitiesSection.tsx
@@ -595,7 +595,7 @@ const ActivitiesSection = () => {
                                 className="w-full sm:w-auto bg-[#DBD4CE] hover:bg-[#DBD4CE] text-[#210F0080] rounded-xl px-6 py-3 md:text-lg font-semibold"
                                 onClick={(e) => {
                                   e.stopPropagation();
-                                  navigate(`/program`);
+                                  navigate("/program");
                                 }}
                               >
                                 Pozrieť si program
