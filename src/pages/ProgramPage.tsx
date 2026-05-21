d0b5aef460cb1cbc16e9e8d9a2e8a382d8abfa4a
kdendesi-coder <kdendesi@gmail.com>
2026-05-21 17:44:28 +0200

    Update ProgramPage.tsx

--- src/pages/ProgramPage.tsx
+++ src/pages/ProgramPage.tsx
@@ -21,7 +21,7 @@ const ProgramPage = () => {
 
         <div className="w-full flex justify-center">
           <img
-            src="/public/RC-Sirotar-herna-program_2026-05-768x1086.jpg"
+            src="RC-Sirotar-herna-program_2026-05-768x1086.jpg"
             alt="Program"
             className="w-full max-w-[768px] h-auto object-contain rounded-3xl shadow-lg"
           />
