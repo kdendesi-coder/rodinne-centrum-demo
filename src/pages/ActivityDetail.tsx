799e19448ea2a4f375c283e45a9bf53e5e9e9a4d
kdendesi-coder <kdendesi@gmail.com>
2026-06-13 10:02:28 +0200

    Update ActivityDetail.tsx

--- src/pages/ActivityDetail.tsx
+++ src/pages/ActivityDetail.tsx
@@ -592,7 +592,7 @@ const ActivityDetail = () => {
 
             {/* Google form */}
             {activity.googleFormLink && (
-              <div className="mt-6 flex flex-col sm:flex-row gap-6">
+              <div className="mt-6 flex flex-col sm:flex-row gap-4">
                 <a href={activity.googleFormLink} target = "_blank">
                   <Button className="rounded-xl px-6 py-3 text-base md:text-lg font-semibold">
                     Prihlasit sa
