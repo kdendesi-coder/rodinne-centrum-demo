93424e5204ca368de5db66f11e7d7302198df6ef
kdendesi-coder <kdendesi@gmail.com>
2026-06-13 09:55:24 +0200

    Update ActivityDetail.tsx

--- src/pages/ActivityDetail.tsx
+++ src/pages/ActivityDetail.tsx
@@ -601,19 +601,19 @@ const ActivityDetail = () => {
               </div>
             )}
 
-
-            <Button
-                                variant="secondary"
-                                className="w-full sm:w-auto bg-[#DBD4CE] hover:bg-[#DBD4CE] text-[#210F0080] rounded-xl px-6 py-3 md:text-lg font-semibold"
-                                onClick={(e) => {
-                                  e.stopPropagation();
-                                  navigate("/program");
-                                }}
-                              >
-                                Pozrieť si program
-                              </Button>
-
         </div>
+            <Button
+              variant="secondary"
+              className="w-full sm:w-auto bg-[#DBD4CE] hover:bg-[#DBD4CE] text-[#210F0080] rounded-xl px-6 py-3 md:text-lg font-semibold"
+              onClick={(e) => {
+                e.stopPropagation();
+                navigate("/program");
+              }}
+              >
+                Pozrieť si program
+              </Button>
+
+        
       </main>
       <Footer />
 
